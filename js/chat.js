59b55cd34778062884c1924014062fcdb2994f25
alexanderpiasahasiamah-png <alexanderpiasahasiamah@gmail.com>
2026-02-04 00:01:01 +0000

    Create chat.js

---
+++ js/chat.js
@@ -0,0 +1,8 @@
+function openChat(){
+  document.getElementById("chatBox").style.display="block";
+}
+
+function sendMsg(){
+  let msg=document.getElementById("msg").value;
+  alert("PulseX AI is analyzing: " + msg);
+}
